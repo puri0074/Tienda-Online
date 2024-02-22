666594e818ffb16f813b27177b61e9e172dac0d8
practica2024 <puri0074.com>
2024-02-22 18:21:04 -0500

    Agregamos Bootstrap y Css

--- index..php
+++ index..php
@@ -24,9 +24,13 @@ $resultado = $sql->fetchaAll(PDO::FETCH_ASSOC);
     <!--Titulo de la pagina-->
     <title>Tienda Online</title>
     
-     <!---->
+     <!--Agregamos Bootstrap y css -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/estilos.css" rel="stylesheet">
 </head>
+
 <body>
+    
     
 </body>
 </html>
